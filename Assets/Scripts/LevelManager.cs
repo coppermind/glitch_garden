459f4029f5c277adb770e06796754e9054e882e1
Paul Corpuz <paul.corpuz@gmail.com>
2015-02-19 23:43:59 +0800

    Imported package from the course. Added splash and start screens.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfterTime;
+	public bool autoLoadNextLevel = false;
+	
+	void Start() {
+		if (autoLoadNextLevel) {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfterTime);
+		}
+	}
+
+	public void LoadLevel(string name){
+		Application.LoadLevel(name);
+	}
+	
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+	public void QuitRequest(){
+		Application.Quit();
+	}
+
+}
---
+++ Assets/Scripts/SplashController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashController : MonoBehaviour {
+
+	void Start () {
+		Invoke ("LoadStartScreen", 4f);
+	}
+	
+	void LoadStartScreen() {
+		Application.LoadLevel(1);
+	}
+	
+}
