ffa22d1613b45e292d720a17baaffbaa1bfb32d2
Paul Corpuz <paul.corpuz@gmail.com>
2015-02-20 16:14:44 +0800

    Added level canvas prefab and level scenes

---
+++ Assets/Scripts/FadeInController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeInController : MonoBehaviour {
+
+	void Start () {
+		Debug.Log ("A: " + GetComponent<Image>().color.a);
+		Invoke("Suicide", 0.9f);
+	}
+	
+	void Suicide() {
+		Debug.Log("Killing self now");
+		Destroy(gameObject);
+	}
+}
