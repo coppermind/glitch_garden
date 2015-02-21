21a99298c87f7476648a3a617232d9141bdc624d
Paul Corpuz <paul.corpuz@gmail.com>
2015-02-22 01:42:13 +0800

    Removed smuggler npc. Added attacker script to fox and lizard npcs

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+	[Range (-2f, 1.5f)]
+	public float walkSpeed;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+	}
+}
