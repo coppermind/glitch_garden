8249d18e98838e46267811dbc295622f00fe6676
Paul Corpuz <paul.corpuz@gmail.com>
2015-02-22 13:57:30 +0800

    Added animation events to attackers

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,32 +4,39 @@ using System.Collections;
 public class Attacker : MonoBehaviour {
 
 	[Range (-2f, 1.5f)]
-	public float walkSpeed;
+	public float currentSpeed;
 	public float hitPoints;
 	public float damagePoints;
 	
-	private bool isWalking = true;
-	private bool isAttacking = false;
-
-	void Start () {
+//	private bool isWalking = true;
+//	private bool isAttacking = false;
+	private Animator animator;
 	
+	void Start () {
+		animator = GetComponent<Animator>();
 	}
 	
 	void Update () {
-		if (isWalking) {
-			transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
-		}
+		transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
 	}
 	
 	void OnTriggerEnter2D(Collider2D collider) {
 		if (collider.gameObject.tag == "Defender") {
 			Debug.Log("Hit a defender");
-			isWalking = false;
-			isAttacking = true;
+//			isWalking = false;
+//			isAttacking = true;
 			//GameObject children = gameObject.GetComponentsInChildren<GameObject>() as GameObject;
 			
 		} else if (collider.gameObject.tag == "Projectile") {
 			Debug.Log("Hit by a projectile");
 		}
+	}
+	
+	public void SetSpeed(float speed) {
+		currentSpeed = speed;
+	}
+	
+	public void AttackCurrentTarget(float damage) {
+		Debug.Log("Attacking current target with " + damage.ToString() + " amount of damage");
 	}
 }
