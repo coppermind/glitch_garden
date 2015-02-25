155a73b68096a2c0f8c3a058c759f210f4a22b06
Paul Corpuz <paul.corpuz@gmail.com>
2015-02-25 14:11:30 +0800

    explicitly required health component for defender entities.

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -1,16 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof (Health))]
 public class Defender : MonoBehaviour {
 
-	void Start () {
-	
-	}
-	
-	void Update () {
-	
-	}
-	
 	void OnTriggerEnter2D(Collider2D collider) {
 		if (collider.gameObject.tag == "Attacker") {
 		
