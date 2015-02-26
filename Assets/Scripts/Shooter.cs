632a0ab481e9a5957bdda5dfba94e07905923079
Paul Corpuz <paul.corpuz@gmail.com>
2015-02-27 02:10:01 +0800

    Removed Projectiles layer prefab. Projectile parent now created dynamically by gameObjects if parent layer is missing.

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,11 +1,24 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class Shooter : MonoBehaviour {
 
 	public GameObject projectile;
-	public GameObject parent;
 	public GameObject gun;
+	
+	private GameObject parent;
+	
+	void Start() {
+		FindOrSetupParent();
+	}
+	
+	private void FindOrSetupParent() {
+		parent = GameObject.Find("Projectiles");
+		if (null == parent) {
+			parent = new GameObject("Projectiles");
+		}
+	}
 	
 	private void Fire() {
 		GameObject newProjectile = Instantiate(projectile) as GameObject;
