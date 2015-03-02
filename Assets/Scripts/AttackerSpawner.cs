b30b64177ca4d94fec555b0e82b3d3e07c89d96a
Paul Corpuz <paul.corpuz@gmail.com>
2015-03-02 23:10:26 +0800

    Refactored attacker spawner!

--- Assets/Scripts/AttackerSpawner.cs
+++ Assets/Scripts/AttackerSpawner.cs
@@ -1,22 +1,11 @@
 ﻿using UnityEngine;
-using System;
 using System.Collections;
 
 public class AttackerSpawner : MonoBehaviour {
 
 	public GameObject[] attackers;
 	
-	private float[] lastSpawnTime;
-	private int currentAttacker;
-	
-	void Start() {
-		lastSpawnTime = new float[attackers.Length];
-	}
-
 	void Update () {
-		Debug.Log("Spawning: " + UnityEngine.Random.Range(0, attackers.Length * 100) % attackers.Length);
-		Debug.Log("Time Array: " + lastSpawnTime.Length);
-		Debug.Log("First: " + lastSpawnTime[0]);
 		foreach (GameObject thisAttacker in attackers) {
 			if (isTimeToSpawn(thisAttacker)) {
 				Spawn(thisAttacker);
@@ -24,21 +13,18 @@ public class AttackerSpawner : MonoBehaviour {
 		}
 	}
 	
-	int GetCurrentAttacker() {
-		currentAttacker = UnityEngine.Random.Range(0, attackers.Length * 100) % attackers.Length;
-		return currentAttacker;
-	}
-	
-	bool isTimeToSpawn(GameObject attacker) {
-		if (0 == lastSpawnTime[currentAttacker]) {
-			lastSpawnTime[currentAttacker] = Time.time;
-			return true;
+
+	bool isTimeToSpawn(GameObject attackerGameObject) {
+		Attacker attackerComponent = attackerGameObject.GetComponent<Attacker>();
+		
+		float meanSpawnDelay = attackerComponent.seenEverySeconds;
+		float spawnsPerSecond = 1 / meanSpawnDelay;
+		float threshold = spawnsPerSecond * Time.deltaTime / 10;
+		
+		if (Time.deltaTime > meanSpawnDelay) {
+			Debug.LogWarning("Swan rate capped by framerate");
 		} else {
-			Attacker attackerComponent = attackers[currentAttacker].GetComponent<Attacker>();
-			
-			float elapsedTime = Time.time - lastSpawnTime[currentAttacker];
-			if (attackerComponent.seenEverySeconds < elapsedTime) {
-				lastSpawnTime[currentAttacker] = Time.time;
+			if (Random.value < threshold) {
 				return true;
 			}
 		}
